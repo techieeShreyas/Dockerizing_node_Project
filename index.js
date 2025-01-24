0da0120f6823f7ee3006a7af4fcb557fcd130749
techieShreyas <shreyasagrawaltechie@gmail.com>
2025-01-24 10:06:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/home", (req, res)=>{
+    res.json({message: 'OK'});
+});
+
+app.listen(3000, ()=>{
+    console.log("Started the server");
+});
