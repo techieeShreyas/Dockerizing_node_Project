4084598c46809417299824be88cd77801283aa7b
techieShreyas <shreyasagrawaltechie@gmail.com>
2025-01-24 10:29:14 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 
 app.get("/home", (req, res)=>{
     res.json({message: 'OK'});
 });
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("Started the server");
 });
